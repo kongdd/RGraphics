a6939ae13235a6f6f54b10eb2ca2810772cb21a0
Paul Murrell <paul@stat.auckland.ac.nz>
2008-04-02 00:00:00 +0000

    version 1.0-7

--- R/figures.R
+++ R/figures.R
@@ -383,34 +383,29 @@ for (i in 1:nrow) {
 
 }
 figure3.9 <- function() {
-# On my X11 server, with a UTF-8 locale, there were no 
-# font sets for the symbol font, so I had to set the locale
-# so that a single (symbol) font was searched for instead.
-# Sys.setlocale(locale="en_NZ.iso88591")
-
 par(mar=rep(0, 4), cex=0.7)
 plot.new()
 plot.window(c(0.05, 0.95), 0:1)
-family <- c("sans", "serif", "mono", "symbol")
+family <- c("sans", "serif", "mono")
 face <- 1:4
 for (i in 1:4)
-  for (j in 1:4) {
+  for (j in 1:3) {
     par(family=family[j], lheight=1.5)
     text(seq(.15, .85, length=4)[i],
-         seq(.15, .85, length=4)[j],
+         seq(.25, .75, length=3)[j],
          paste("family=\"", family[j], "\"\nfont=", face[i], sep=""),
          font=face[i])
   }
-segments(.02, c(.04, .27, .5, .73, .96), 
-         .98, c(.04, .27, .5, .73, .96), col="grey")
-segments(.02, c(.04, .27, .5, .73, .96) - .01, 
-         .02, c(.04, .27, .5, .73, .96) + .01, col="grey")
-segments(.98, c(.04, .27, .5, .73, .96) - .01, 
-         .98, c(.04, .27, .5, .73, .96) + .01, col="grey")
+segments(.02, c(.125, .375, .625, .875), 
+         .98, c(.125, .375, .625, .875), col="grey")
+segments(.02, c(.125, .375, .625, .875) - .01, 
+         .02, c(.125, .375, .625, .875) + .01, col="grey")
+segments(.98, c(.125, .375, .625, .875) - .01, 
+         .98, c(.125, .375, .625, .875) + .01, col="grey")
 rect(c(.27, .5, .73) - .01,
-     .02,
+     .1,
      c(.27, .5, .73) + .01,
-     .98, col="white", border=NA)
+     .9, col="white", border=NA)
 
 
 
