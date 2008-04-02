df5e291b407578016eab9df1fe1691d7bdf5797b
Paul Murrell <paul@stat.auckland.ac.nz>
2008-04-02 12:34:57 +0000

    version 1.0-7

--- tests/test.R
+++ tests/test.R
@@ -3,7 +3,7 @@ library(RGraphics)
 for (i in ls("package:RGraphics", pattern="^figure")) {
   cat(i, "\n")
   postscript(paste("RGraphicsTest", i, ".ps", sep=""),
-             fonts=c("mono", "sans", "serif", "symbol"))
+             fonts=c("mono", "sans", "serif"))
   try(do.call(i, list()))
   dev.off()
 }
